122f3f60871597b7e0b0b8b4d442d3e83749b9a8
Jonas Ferreira da Paixão <65820187+jonaspaixao@users.noreply.github.com>
2025-10-21 00:27:12 -0300

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+
+# Carregar CSV
+df = pd.read_csv("C:/Users/jonas/Downloads/Fatores_IPCA_IPCA_E_INPC_INCC_IGPM_TR_SELIC_ENCOGE.csv", sep=";")
+
+# Mostrar tabela
+st.title("Tabela de Índices de Correção Monetária")
+st.dataframe(df)
+
