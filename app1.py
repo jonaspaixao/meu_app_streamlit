eeddb7af189d13a2ff4cb08d024b4a7c07d0e421
Jonas Ferreira da Paixão <65820187+jonaspaixao@users.noreply.github.com>
2025-10-21 01:08:17 -0300

    Update app1.py

--- app1.py
+++ app1.py
@@ -6,8 +6,9 @@ df = pd.read_csv("Fatores_IPCA_IPCA_E_INPC_INCC_IGPM_TR_SELIC_ENCOGE.csv", sep="
 
 # Mostrar tabela
 #st.title("Tabela de Índices de Correção Monetária")
-with st.expanded("Tabela de Índices de Correção Monetária")
+with st.expanded("Tabela de Índices de Correção Monetária", expanded=True):
 st.dataframe(df)
+
 
 
 
