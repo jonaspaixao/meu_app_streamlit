89aeaedc08824147ae469eefaf9467fa6c64c5a9
Jonas Ferreira da Paixão <65820187+jonaspaixao@users.noreply.github.com>
2025-10-21 01:15:42 -0300

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,13 +1,24 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Tabela de Índices de Correção Monetária")
+# Título
+st.title("📊 Tabela de Índices de Correção Monetária")
 
 # Carregar CSV
 df = pd.read_csv("Fatores_IPCA_IPCA_E_INPC_INCC_IGPM_TR_SELIC_ENCOGE.csv", sep=";")
 
 # Mostrar tabela
 st.dataframe(df)
+
+# Botão de download abaixo da tabela
+st.download_button(
+    label="⬇️ Baixar arquivo CSV",
+    data=df.to_csv(index=False, sep=";").encode("utf-8"),
+    file_name="indices_correcao.csv",
+    mime="text/csv"
+)
+
+
 
 
 
