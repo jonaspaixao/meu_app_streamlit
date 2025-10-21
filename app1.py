eec8b2aa2eba576f0a47f90e5bae761fa2493a6a
Jonas Ferreira da Paixão <65820187+jonaspaixao@users.noreply.github.com>
2025-10-21 01:10:02 -0300

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,15 +1,14 @@
 import streamlit as st
 import pandas as pd
 
-# Título
 st.title("Tabela de Índices de Correção Monetária")
 
 # Carregar CSV
 df = pd.read_csv("Fatores_IPCA_IPCA_E_INPC_INCC_IGPM_TR_SELIC_ENCOGE.csv", sep=";")
 
-# Mostrar tabela dentro de um expander já aberto
-with st.expander("Visualizar tabela completa", expanded=True):
-    st.dataframe(df)
+# Mostrar tabela
+st.dataframe(df)
+
 
 
 
