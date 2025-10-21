2575b6884711fb961748d165f52362cd77e87030
Jonas Ferreira da Paixão <65820187+jonaspaixao@users.noreply.github.com>
2025-10-21 01:12:15 -0300

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,27 +1,15 @@
 import streamlit as st
 import pandas as pd
 
-# Estilo fullscreen
-st.markdown("""
-    <style>
-        .fullscreen-table .stDataFrame {
-            height: 90vh !important;
-            width: 100% !important;
-        }
-    </style>
-""", unsafe_allow_html=True)
-
-# Título
 st.title("Tabela de Índices de Correção Monetária")
 
 # Carregar CSV
 df = pd.read_csv("Fatores_IPCA_IPCA_E_INPC_INCC_IGPM_TR_SELIC_ENCOGE.csv", sep=";")
 
-# Tabela com classe personalizada
-with st.container():
-    st.markdown('<div class="fullscreen-table">', unsafe_allow_html=True)
-    st.dataframe(df)
-    st.markdown('</div>', unsafe_allow_html=True)
+# Mostrar tabela
+st.dataframe(df)
+
+
 
 
 
